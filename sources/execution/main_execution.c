e80919722413e5719c52b4a673e7044ca387a999
Pauline Mieuzet <pmieuzet@3F3.42angouleme.fr>
2023-02-02 14:19:09 +0100

    add main_execution

--- sources/execution/main_execution.c
+++ sources/execution/main_execution.c
@@ -2,14 +2,17 @@
 
 t_list	*main_execution(t_list *lst, t_env *env)
 {
-	t_command		*tmp;
-
-	tmp = lst->first;
-	while (tmp)
+	if (!lst->first)
+		return (NULL);
+	else if (!lst->first->next)
 	{
-		if (!check_builtins(tmp, env))
+		if (!check_builtins(lst->first, env))
 			printf("not builtin\n");
-		tmp = tmp->next;
+	}
+	else
+	{
+		printf("pipe.s\n");
+		//pipex(lst->first);
 	}
 	return (0);
 }
